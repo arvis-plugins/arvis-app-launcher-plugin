d44d39f57455ff143829c1e6f1a371f963f50be5
jopemachine <jopemachine@naver.com>
2021-05-31 22:21:13 +0900

    Upd

--- darwin.js
+++ darwin.js
@@ -1,32 +1,7 @@
 const plistParser = require("plist");
-const conf = require("conf.json");
-const path = require("path");
 
 const getIcon = () => {
   return "icon.png";
-};
-
-const getItems = async (inputStr) => {
-  const inApplicationFolder = await fse.readdir(conf.applicationFolder.darwin);
-  const apps = _.filter(inApplicationFolder, (appName) =>
-    appName.endsWith(".app")
-  );
-
-  const targetApps = _.filter(apps, (appName) =>
-    appName.toLowerCase().startsWith(inputStr.toLowerCase())
-  );
-
-  return {
-    items: targetApps.map((appName) => {
-      return {
-        title: appName,
-        arg: `${conf.applicationFolder.darwin}${path.sep}${appName}`,
-        icon: {
-          path: `${__dirname}${sep}icons${sep}${getIcon(fileName)}`,
-        },
-      };
-    }),
-  };
 };
 
 // * Need method to import icns to png format
@@ -53,4 +28,4 @@ const getItems = async (inputStr) => {
 //   );
 // };
 
-module.exports = { getItems };
+module.exports = { getIcon };
--- index.js
+++ index.js
@@ -1,22 +1,83 @@
-const { getItems: getDarwinItems } = require("./darwin");
-const { getItems: getWindowsItems } = require("./win32");
+const { getIcon: getWin32Icon } = require("./win32");
+const { getIcon: getDarwinIcon } = require("./darwin");
+const { getIcon: getLinuxIcon } = require("./linux");
+const path = require("path");
+const stringSimilarity = require("string-similarity");
+const conf = require("./conf.json");
+const fg = require("fast-glob");
+const os = require("os");
+const sep = path.sep;
 
-const getPluginItem = async (inputStr) => {
+const getIcon =
+  process.platform === "darwin"
+    ? getDarwinIcon
+    : process.platform === "win32"
+    ? getWin32Icon
+    : getLinuxIcon;
+
+const getTargetPath = (inputStr) => {
   switch (process.platform) {
     case "win32":
-      return getWindowsItems(inputStr);
+      return `${
+        conf.applicationFolder[process.platform]
+      }${sep}**${sep}*${inputStr}*`
+        .split("\\")
+        .join("/");
     case "darwin":
-      return getDarwinItems(inputStr);
-    case "liunx":
-      break;
-    default:
-      return [
-        {
-          title: "Not supported platform!",
-          valid: false,
-        },
-      ];
+      return `${conf.applicationFolder[process.platform]}${sep}*${inputStr}*`;
+    case "linux":
+      return "";
   }
+};
+
+const getPluginItem = async (inputStr) => {
+  return new Promise((resolve, reject) => {
+    const globOpts = {
+      dot: false,
+      cwd: path.parse(process.cwd()).root,
+      absolute: true,
+      caseSensitiveMatch: false,
+      concurrency: conf.concurrency ? conf.concurrency : os.cpus().length,
+      suppressErrors: true,
+      deep: process.platform === 'darwin' ? 1 : conf.deep,
+      onlyFiles: process.platform === 'darwin' ? false : true,
+    };
+
+    const getFileOrDirName = (filePath) => {
+      return filePath.endsWith(sep)
+        ? filePath
+            .substring(0, filePath.length - 1)
+            .split(sep)
+            .pop()
+        : filePath.split(sep).pop();
+    };
+
+    const targetPath = getTargetPath(inputStr);
+
+    fg(targetPath, globOpts)
+      .then((targetApps) => {
+        const items = targetApps.map((appPath) => {
+          const appName = getFileOrDirName(appPath);
+          return {
+            title: appName,
+            subtitle: appPath,
+            arg: appPath,
+            icon: {
+              path: `${__dirname}${sep}icons${sep}${getIcon(appName)}`,
+            },
+            similarity: stringSimilarity.compareTwoStrings(inputStr, appName),
+          };
+        })
+
+        resolve({
+          items: items.sort((a, b) => a.similarity < b.similarity ? 1 : -1),
+        });
+      })
+      .catch((err) => {
+        console.error("arvis-app-launcher-plugin throws an Error", err);
+        resolve({ items: [] });
+      });
+  });
 };
 
 module.exports = getPluginItem;
